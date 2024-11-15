6579330e2ca7bd17b28fbf938adc0f0413e65c3c
Rongbin Zhong <zhongrongbin2021@163.com>
2024-11-15 23:49:55 +0800

    继续整合服务器，完成http回调和编写和open连接回调的编写

--- src/server.hpp
+++ src/server.hpp
@@ -136,10 +136,36 @@ public:
         }
 
         // 2.判断session是否存在
+        std::string val;
+        if (get_cookie_val(cookie_str, "SSID", val) == false)
+        {
+            DLOG("session id信息不存在");
+            http_response(conn, websocketpp::http::status_code::bad_request, false, "session id不存在，请重新登录");
+        }
 
         // 3.通过session去获取会话的信息
+        session_ptr ssp = _session_manager.get_session_by_id(std::stol(val));
+        if (ssp.get() == nullptr)
+        {
+            DLOG("session信息不存在");
+            http_response(conn, websocketpp::http::status_code::bad_request, false, "session信息不存在，请重新登录");
+        }
 
-        // 4.通过会话信息获取用户信息
+        // 4.从数据库中通过会话信息获取用户信息
+        uint64_t uid = ssp->get_user_id();
+        Json::Value user_info;
+        if (_user_table.select_by_id(uid, user_info) == false)
+        {
+            DLOG("数据库中不存在该用户信息");
+            http_response(conn, websocketpp::http::status_code::bad_request, false, "找不到用户信息，请重新登录");
+        }
+        std::string body;
+        json_util::serialize(user_info, body);
+        conn->set_body(body);
+        conn->append_header("Content-Type", "application/json");
+        conn->set_status(websocketpp::http::status_code::ok);
+        // 5.刷新session过期时间
+        _session_manager.set_session_expire_time(ssp->get_sesssion_id(), SESSION_TIMEOUT);
     }
 
     // 返回默认界面，即登录界面
@@ -191,10 +217,51 @@ public:
         }
     }
 
+    void open_game_hall(server_t::connection_ptr &conn)
+    {
+        Json::Value response;
+        session_ptr ssp = get_session_by_cookie(conn);
+        if (ssp.get() == nullptr)
+        {
+            return;
+        }
+        // 判断是否重复登录
+        if (_online_manager.is_in_game_hall(ssp->get_user_id()) || _online_manager.is_in_game_room(ssp->get_user_id()))
+        {
+            response["optype"] = "hall_ready";
+            response["result"] = false;
+            response["reason"] = "重复登录";
+            return server_response(conn, response);
+        }
+        // 将登录用户添加进管理用户模块
+        _online_manager.login_game_hall(ssp->get_user_id(), conn);
+        response["optype"] = "hall_ready";
+        response["result"] = true;
+        server_response(conn, response);
+        // 设置session为永久存在
+        _session_manager.set_session_expire_time(ssp->get_user_id(), SESSION_TIMEOUT);
+        return;
+    }
+
+    void open_game_room(server_t::connection_ptr &conn)
+    {
+    }
+
     /////
     // open连接成功处理回调函数
     void handler_open(websocketpp::connection_hdl hdl)
     {
+        server_t::connection_ptr conn = _server.get_con_from_hdl(hdl);
+        websocketpp::http::parser::request request = conn->get_request();
+        std::string url = request.get_uri();
+        if (url == "/hall")
+        {
+            return open_game_hall(conn);
+        }
+        else if (url == "/room")
+        {
+            return open_game_room(conn);
+        }
     }
 
     /////
@@ -211,16 +278,71 @@ public:
 
 private:
     // 从cookie信息中提取session的id
-    bool get_cookie_val(const std::string &cookie_str, const std::string &key, const std::string &val)
+    bool get_cookie_val(const std::string &cookie_str, const std::string &key, std::string &val)
     {
-        // Cookie:SSID=XXX;path=/
+        // Cookie:SSID=XXX;path=/;
         std::vector<std::string> cookie_arr;
         string_util::split(cookie_str, ";", cookie_arr);
-        for(auto& tmp:cookie_arr)
+        for (auto &tmp : cookie_arr)
         {
-            
-
+            std::vector<std::string> ssd_arr;
+            size_t ret = string_util::split(tmp, "=", ssd_arr);
+            if (ret != 2)
+            {
+                continue;
+            }
+            if (ssd_arr[0] == key)
+            {
+                val = ssd_arr[1];
+                return true;
+            }
         }
+        return false;
+    }
+
+    // 将json结构化数据发送给客户端
+    void server_response(server_t::connection_ptr &conn, const Json::Value &response)
+    {
+        std::string body;
+        json_util::serialize(response, body);
+        conn->send(body);
+    }
+
+    // 通过cookie找到session
+    session_ptr get_session_by_cookie(server_t::connection_ptr &conn)
+    {
+        Json::Value err_response; // 用于返回错误信息
+        // 1.查找cookie信息
+        std::string cookie_str = conn->get_request_header("Cookie");
+        if (cookie_str.empty() == true)
+        {
+            err_response["optype"] = "hall_ready";
+            err_response["result"] = false;
+            err_response["reason"] = "找不到cookie信息，请重新登录";
+            server_response(conn, err_response);
+            return session_ptr();
+        }
+        // 从cookie信息中找到session
+        std::string ssid_str;
+        if (get_cookie_val(cookie_str, "SSID", ssid_str) == false)
+        {
+            err_response["optype"] = "hall_ready";
+            err_response["result"] = false;
+            err_response["optype"] = "找不到session信息，请重新登录";
+            server_response(conn, err_response);
+            return session_ptr();
+        }
+        // 在session管理中查找对应的会话信息
+        session_ptr ssp = _session_manager.get_session_by_id(std::stol(ssid_str));
+        if (ssp.get() == nullptr)
+        {
+            err_response["optype"] = "hall_ready";
+            err_response["result"] = false;
+            err_response["reason"] = "找不到session信息，请重新登录";
+            server_response(conn, err_response);
+            return session_ptr();
+        }
+        return ssp;
     }
 
 private:
